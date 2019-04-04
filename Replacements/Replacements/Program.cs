daa241e6f7a3d5c202e5c957746bdc99aa57ff3c
kmitchell394 <46894969+kmitchell394@users.noreply.github.com>
2019-04-04 13:09:00 -0400

    Add files via upload

---
+++ Replacements/Replacements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using ModernCR;
+
+namespace Replacements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cr = new RunCrystal();
+            cr.BuildReport("D:\\Programs\\Replacement\\Replacements.rpt", "D:\\Programs\\Replacement\\Replacements.pdf");
+
+            cr.SetToAddress("k.mitchell@teammodern.com");
+            cr.SetToAddress("j.lyons@teammodern.com");
+
+            cr.SetSubject("Replacements " + DateTime.Now.ToString("MM-dd"));
+            cr.SetAttachments("D:\\Programs\\Replacement\\Replacements.pdf");
+            cr.SendEmail();
+        }
+    }
+}
